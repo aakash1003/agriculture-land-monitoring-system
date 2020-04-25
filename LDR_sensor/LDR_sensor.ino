4d860d9121e4bd4b9715c51141de229388634890
Aakash Sharma <sharma1032000@gmail.com>
2020-04-25 10:18:21 +0530

    added code for sunlight detection

---
+++ LDR_sensor/LDR_sensor.ino
@@ -0,0 +1,32 @@
+int msensor= A1;
+int msvalue=0;
+int led=13;
+//boolean flag=false;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(msensor,INPUT);
+  pinMode(led,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  msvalue = analogRead(msensor);
+  Serial.println(msvalue);
+
+  if(msvalue==1) //&& (flag == false))
+  {
+    digitalWrite(led,HIGH);
+   // flag = true;
+    delay(1000);
+  }
+
+  if(msvalue==0)// && (flag == true))
+  {
+    digitalWrite(led,LOW);
+  //  flag = false;
+    delay(1000);
+  }
+
+  delay(1000);
+}
